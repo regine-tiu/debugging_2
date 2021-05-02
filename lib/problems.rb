c62b15e6b06c966dcac3bcf3544b8a23a231b761
regine-tiu <regine.d.tiu@gmail.com>
2021-05-02 18:18:26 +0800

    second commit

---
+++ lib/problems.rb
@@ -0,0 +1,32 @@
+def largest_prime_factor(num)
+  num.downto(2) do |factor|
+   if num % factor == 0 && prime?(factor)
+    return factor
+  end  
+end  
+
+end  
+
+def prime?(num)
+  return false if num < 2
+  (2...num).none? {|i| num % i == 0}  
+end  
+
+def unique_chars?(str)
+ deja_vu = []
+ str.each_char do |char|
+   if deja_vu.include?(char)
+    return false
+   end
+   deja_vu << char
+ end  
+  true
+end  
+
+def dupe_indices(arr)
+tracker = Hash.new{|h,k| h[k] = [] }
+ arr.each_with_index do |ele, i|
+    tracker[ele] << i
+ end 
+ tracker.select {|ele, arr| arr.length > 1}
+end  
